c61ce46115bc157b377a06ca38ad196ac2537c2f
jobe2208 <jobe2208@student.miun.se>
2024-10-21 10:49:55 +0200

    divide by zero error
    added a  if statement to catch  divide by zero errors
    
    also renamed the variables making it more clear what values they hold

--- calculate_score.py
+++ calculate_score.py
@@ -1,27 +1,37 @@
 def calculate_score(employee_scores, all_scores):
-    base = 1 # Needs to be base 1 to avoid division by 0 in case of empty requirements
-    must  = base
-    should = base
-    merit = base
-    emp_must = base
-    emp_should = base
-    emp_merit = base
+    base = 0  # Using Axel calculation method change the value on base to 1 and vice versa if are using Johannes method
+
+    must_req = base # N amount of must-have skills
+    should_req = base
+    merit_req = base
+
+    matched_must = base # N amount of must-have skills matched
+    matched_should = base
+    matched_merit = base
+
     for score in all_scores:
         if score[1] == '3':
-            must+=1
+            must_req += 1
         if score[1] == '2':
-            should+=1
+            should_req += 1
         if score[1] == '1':
-            merit+=1
-    
+            merit_req += 1
+
     for score in employee_scores:
         if score == '3':
-            emp_must+=1
+            matched_must += 1
         if score == '2':
-            emp_should+=1
+            matched_should = 1
         if score == '1':
-            emp_merit+=1
-    
-    #score = emp_must/must*(75*emp_should/should + 25*emp_merit/merit)      # Axel calculation method
-    score = emp_must/must*60 + emp_should/should*30 + emp_merit/merit*10    # Johannes calculation method
+            matched_merit += 1
+
+    if must_req == 0: # Avoid division by zero
+        must_req = 1
+    if should_req == 0:
+        should_req = 1
+    if merit_req == 0:
+        merit_req = 1
+
+    # score = must*(75*emp_should/should + 25*emp_merit/merit)  # Axel calculation method
+    score = matched_must / must_req * 60 + matched_should / should_req * 30 + matched_merit / merit_req * 10  # Johannes calculation method
     return f"{round(score)}%"
